a1d5a337981bd6e5852b3cfb96c035aea9c58181
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-28 11:12:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = {
+        prefix: 'g!',
+        token: config.Token
+    };
+const { GiveawaysManager } = require('discord-giveaways');
+const manager = new GiveawaysManager(client, {
+    storage: './storage.json',
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: ['MANAGE_MESSAGES', 'ADMINISTRATOR'],
+        embedColor: '#FF0000',
+        embedColorEnd: '#0000FF',
+        reaction: '🎉'
+    }
+});
+client.giveawaysManager = manager;
+
+client.on('ready', () => {
+    console.log('I\'m ready!');
+    client.user.setActivity('giveaways', ({
+     Type: "PLAYING" 
+}));
+});
+
+client.on('message', (message) => {
+    const ms = require('ms');
+    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'start') {
+        client.giveawaysManager.start(message.channel, {
+            time: ms(args[0]),
+            winnerCount: parseInt(args[1]),
+            prize: args.slice(2).join(' ')
+        })
+    }
+
+    if (command === 'reroll') {
+        const messageID = args[0];
+        client.giveawaysManager.reroll(messageID).then(() => {
+            message.channel.send('Giveaway rerolled!');
+        }).catch(() => {
+            message.channel.send('I couldn't find any giveaway on that message id');
+        });
+    }
+
+    if (command === 'edit') {
+        const messageID = args[0];
+        client.giveawaysManager.edit(messageID, {
+            addTime: 5000,
+            newWinnerCount: 3,
+            newPrize: 'New Prize!'
+        }).then(() => {
+            // Here, we can calculate the time after which we are sure that the lib will update the giveaway
+            const numberOfSecondsMax = client.giveawaysManager.options.updateCountdownEvery / 1000;
+            message.channel.send('Success! Giveaway will updated in less than ' + numberOfSecondsMax + ' seconds.');
+        }).catch(() => {
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+
+    if (command === 'delete') {
+        const messageID = args[0];
+        client.giveawaysManager.delete(messageID).then(() => {
+            message.channel.send('Success! Giveaway deleted!');
+        }).catch(() => {
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+    
+    if (command === 'end') {
+        const messageID = args[0];
+        client.giveawaysManager.end(messageID).then(() => {
+            message.channel.send('Success! Giveaway ended!');
+        }).catch(() => {
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+});
+
+client.login(settings.token);
