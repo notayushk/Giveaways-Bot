8a4b213d33a7f98c69a939d1881e2df7afd72edb
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-31 15:36:44 +0530

    Update giveaway.js

--- commands/giveaways/giveaway.js
+++ commands/giveaways/giveaway.js
@@ -1,1 +1,18 @@
+module.exports = {
+name: "giveaway",
+usage: "g!giveaway 1d 1 nitro",
+ownerOnly: false, 
+cooldown: 10000,
+botPermission: [],
+authorPermission: [MANAGE_GUILD],
+aliases: [gstart,start],
+description: "start a giveaway",
+run: async(message,args) => {
+client.giveawaysManager.start(message.channel, {
+            time: ms(args[0]),
+            winnerCount: parseInt(args[1]),
+            prize: args.slice(2).join(' ')
+        }
+}
 
+    
