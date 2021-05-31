a0042d45bac4316f57a79900e0b8d8303be2d60b
Ayush <80032577+Ayush4385@users.noreply.github.com>
2021-05-31 15:57:33 +0530

    Update edit.js

--- commands/giveaways/edit.js
+++ commands/giveaways/edit.js
@@ -8,11 +8,18 @@ authorPermission: [MANAGE_GUILD],
 aliases: [gstart,start],
 description: "start a giveaway",
 run: async(message,args) => {
-client.giveawaysManager.start(message.channel, {
-            time: ms(args[0]),
-            winnerCount: parseInt(args[1]),
-            prize: args.slice(2).join(' ')
-        }
+const messageID = args[0];
+        client.giveawaysManager.edit(messageID, {
+            addTime: ms(args[0]),
+            newWinnerCount: parseInt(args[1]) ,
+            newPrize: 'args.slice(2).join(' ')'
+        }).then(() => {
+            // Here, we can calculate the time after which we are sure that the lib will update the giveaway
+            const numberOfSecondsMax = client.giveawaysManager.options.updateCountdownEvery / 1000;
+            message.channel.send('Giveaway will updated in less than ' + numberOfSecondsMax + ' seconds.');
+        }).catch(() => {
+            message.channel.send('I couldn't find any giveaway on that message id.');
+        });
 }
 
     
